323950df255b7f6e70234eafc0b4181ef67def00
codershona <falgunimst95@gmail.com>
2020-03-03 21:01:27 +0100

    trying to solve problem

--- src/App.js
+++ src/App.js
@@ -19,9 +19,9 @@ class App extends Component {
       <h1>Hi, This is my a React App </h1>
     <p> This is now really working! </p>
     
-    <Person />
-     <Person />
-      <Person />
+    <Person name="Lissa" age="32" />
+     <Person name="Lily" age="23" />
+      <Person name="Lima" age="46" />
     </div>
   );
 
