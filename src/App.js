c45ce3b7efde412a0fb7de7f5e3d7e58e2197dd7
codershona <falgunimst95@gmail.com>
2020-03-04 10:06:52 +0100

    understanding and using state

--- src/App.js
+++ src/App.js
@@ -13,15 +13,27 @@ import Person from './Person/Person'; // use Uppercase character for components.
 // }
 
 class App extends Component {
+   state = {
+    persons: [
+    { name: 'Max', age: 28 }, 
+    { name: 'Manu', age: 29 },
+    { name: 'Stephanie', age: 26 }
+
+    ]
+
+   }
+
+
 	render() {
 	return (
     <div className="App">
       <h1>Hi, This is my a React App </h1>
     <p> This is now really working! </p>
     
-    <Person name="Lissa" age="32" />
-     <Person name="Lily" age="23" >My Hobbies: Biking</Person>
-      <Person name="Lima" age="46" />
+    <button>Switch Name</button>
+    <Person name={this.state.persons[0].name} age={this.state.persons[0].nage} />
+     <Person name={this.state.persons[1].name} age={this.state.persons[1].age} >My Hobbies: Biking</Person>
+      <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
     </div>
   );
 
