8e3f5e922f1b5c35b8e297f041146db49706efcf
codershona <falgunimst95@gmail.com>
2020-03-05 17:55:23 +0100

    working on jsx code means javascript html

--- src/App.js
+++ src/App.js
@@ -9,7 +9,8 @@ class App extends Component {
       { name: 'Manu', age: 29 },
       { name: 'Stephanie', age: 26 }
     ],
-    otherState: 'some other value'
+    otherState: 'some other value',
+    showPersons: false
   }
 
   switchNameHandler = (newName) => {
@@ -34,6 +35,12 @@ class App extends Component {
     } )
   }
 
+
+    togglePersonsHandler = () => {
+      
+    }
+
+
   render () {
  // working with inline styles
   const style = {
@@ -50,8 +57,11 @@ class App extends Component {
         <p>This is really working!</p>
         <button
         style={style} 
-        onClick={() => this.switchNameHandler('Maximilian!!')}>Switch Name</button>
-        <Person 
+        onClick={this.togglePersonsHandler}>Switch Name</button>
+        
+    <div>
+
+    <Person 
           name={this.state.persons[0].name} 
           age={this.state.persons[0].age} />
         <Person 
@@ -63,6 +73,11 @@ class App extends Component {
           name={this.state.persons[2].name} 
           age={this.state.persons[2].age} />
       </div>
+
+
+    </div>
+
+        
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
   }
