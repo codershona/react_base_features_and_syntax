9dabeb4d5659cb0fe2a63c38c7116e930700cdb4
codershona <falgunimst95@gmail.com>
2020-03-06 18:48:52 +0100

    setting class names dynamically done by deleting lists className gets red and bold

--- src/App.js
+++ src/App.js
@@ -84,13 +84,19 @@ class App extends Component {
      }
 
    
-     let classes = ['red', 'bold'].join(' '); 
+     const classes = [];
+     if (this.state.persons.length <= 2) {
+      classes.push('red'); 
+     }
+     if (this.state.persons.length <= 1) {
+      classes.push('bold');
+     }
  
 
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
-        <p className={classes}>This is really working!</p>
+        <p className={classes.join(' ')}>This is really working!</p>
         <button
         style={style} 
         onClick={this.togglePersonsHandler}>TOGGLE Persons</button>
