3de5879caaf050185b3bca923f87814cde96bd78
codershona <falgunimst95@gmail.com>
2020-03-08 15:59:35 +0100

    working with react developer tools and using error boundaries , we must use error boundaries in some case

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import classes from './App.css';
 import Person from './Person/Person';
 // import styled from 'styled-components';
 
+import ErrorBoundary from './ErrorBoundary/ErrorBoundary';
+
 // import Radium, { StyleRoot } from 'radium';
 
 class App extends Component {
@@ -64,14 +66,16 @@ class App extends Component {
 
       <div>
          {this.state.persons.map((person, index) => {
-          return <Person
+          return <ErrorBoundary key={person.id}>
+          <Person
           click={() => this.deletePersonHandler(index)}
             name={person.name} 
             age={person.age} 
             
-            key={person.id}
+           
             changed={(event) => this.nameChangedHandler(event, person.id)} />
-         })}
+            </ErrorBoundary>
+         } )}
     
         </div>
 
